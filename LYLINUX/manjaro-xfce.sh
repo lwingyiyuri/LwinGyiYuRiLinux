3c8e56d70fb65d4f127119c15aa4999385f04f50
lwingyiyuri <114618932+lwingyiyuri@users.noreply.github.com>
2023-01-24 14:16:06 +0630

    Create manjaro-xfce.sh

---
+++ LYLINUX/manjaro-xfce.sh
@@ -0,0 +1,440 @@
+sleep 5 &
+pid=$!
+          #LWINGYI && YURI#1
+          printf "\r W"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r We"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r Wel"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r Welc"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r Welco"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r Welcom"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  "
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome T  "
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To M"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Ma"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Man"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manj"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manja"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjar"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro L"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro Li"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro Lin"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro Linu"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To Manjaro Linux"
+          sleep 0.5
+echo
+          #LWINGYI && YURI#1
+          printf "\r M"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r My"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r My H"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r My He"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r My Hea"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r My Hear"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart F"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart Fo"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart For"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart For Y"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart For Yu"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart For YuR"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r My Heart For YuRi"
+          sleep 0.5
+echo
+
+sleep 5 &
+pid=$!
+          #LWINGYI && YURI#1
+          printf "\r S"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r SC"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r SCR"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r SCRI"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r SCRIP"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r SCRIPT"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r SCRIPT©"
+          sleep 0.5
+          #LWINGYI && YURI#8
+          printf "\r SCRIPT©B"
+          sleep 0.5
+          #LWINGYI && YURI#9
+          printf "\r SCRIPT©BY"
+          sleep 0.5
+          #LWINGYI && YURI#10
+          printf "\r SCRIPT©BY L"
+          sleep 0.5
+          #LWINGYI && YURI#11
+          printf "\r SCRIPT©BY Lw"
+          sleep 0.5
+          #LWINGYI && YURI#12
+          printf "\r SCRIPT©BY Lwi"
+          sleep 0.5
+          #LWINGYI && YURI#13
+          printf "\r SCRIPT©BY Lwin"
+          sleep 0.5
+          #LWINGYI && YURI#14
+          printf "\r SCRIPT©BY LwinG"
+          sleep 0.5
+          #LWINGYI && YURI#15
+          printf "\r SCRIPT©BY LwinGy"
+          sleep 0.5
+          #LWINGYI && YURI#16
+          printf "\r SCRIPT©BY LwinGyi"
+          sleep 0.5
+          #LWINGYI && YURI#17
+          printf "\r SCRIPT©BY LwinGyi&"
+          sleep 0.5
+          #LWINGYI && YURI#18
+          printf "\r SCRIPT©BY LwinGyi&&"
+          sleep 0.5
+          #LWINGYI && YURI#19
+          printf "\r SCRIPT©BY LwinGyi&&Y"
+          sleep 0.5
+          #LWINGYI && YURI#20
+          printf "\r SCRIPT©BY LwinGyi&&Yu"
+          sleep 0.5
+          #LWINGYI && YURI#21
+          printf "\r SCRIPT©BY LwinGyi&&YuR"
+          sleep 0.5
+          #LWINGYI && YURI#22
+          printf "\r SCRIPT©BY LwinGyi&&YuRi"
+          sleep 0.5
+          #LWINGYI && YURI#23
+          printf "\r SCRIPT©BY LwinGyi&&YuRi "
+          sleep 0.5
+          echo 
+          #LWINGYI && YURI#24
+          printf "\r T"
+          sleep 0.5
+          #LWINGYI && YURI#25
+          printf "\r Th"
+          sleep 0.5
+          #LWINGYI && YURI#26
+          printf "\r Tha"
+          sleep 0.5
+          #LWINGYI && YURI#27
+          printf "\r Than"
+          sleep 0.5
+          #LWINGYI && YURI#28
+          printf "\r Thank"
+          sleep 0.5
+          #LWINGYI && YURI#29
+          printf "\r Thanks"
+          sleep 0.5
+          #LWINGYI && YURI#30
+          printf "\r Thanks F"
+          sleep 0.5
+          #LWINGYI && YURI#31
+          printf "\r Thanks Fo"
+          sleep 0.5
+          #LWINGYI && YURI#32
+          printf "\r Thanks For"
+          sleep 0.5
+          #LWINGYI && YURI#33
+          printf "\r Thanks For U"
+          sleep 0.5
+          #LWINGYI && YURI#34
+          printf "\r Thanks For Us"
+          sleep 0.5
+          #LWINGYI && YURI#35
+          printf "\r Thanks For Use"
+          sleep 0.5
+          #LWINGYI && YURI#36
+          printf "\r Thanks For Use T"
+          sleep 0.5
+          #LWINGYI && YURI#37
+          printf "\r Thanks For Use Th"
+          sleep 0.5
+          #LWINGYI && YURI#38
+          printf "\r Thanks For Use Thi"
+          sleep 0.5
+          #LWINGYI && YURI#39
+          printf "\r Thanks For Use This"
+          sleep 0.5
+          #LWINGYI && YURI#40
+          printf "\r Thanks For Use This "
+          sleep 0.5
+          #LWINGYI && YURI#41
+          printf "\r Thanks For Use This "
+          sleep 0.5
+          #LWINGYI && YURI#42
+          printf "\r Thanks For Use This "
+          sleep 0.5
+          #LWINGYI && YURI#43
+          printf "\r Thanks For Use This S"
+          sleep 0.5
+          #LWINGYI && YURI#44
+          printf "\r Thanks For Use This Sc"
+          sleep 0.5
+          #LWINGYI && YURI#45
+          printf "\r Thanks For Use This Scr"
+          sleep 0.5
+          #LWINGYI && YURI#46
+          printf "\r Thanks For Use This Scri"
+          sleep 0.5
+          #LWINGYI && YURI#47
+          printf "\r Thanks For Use This Scrip"
+          sleep 0.5
+          #LWINGYI && YURI#48
+          printf "\r Thanks For Use This Script"
+          sleep 0.5
+          #LWINGYI && YURI#49
+          echo
+          printf "\r ©"
+          sleep 0.5
+          #LWINGYI && YURI#50
+          printf "\r ©B"
+          sleep 0.5
+          #LWINGYI && YURI#51
+          printf "\r  ©By"
+          sleep 0.5
+          #LWINGYI && YURI#52
+          printf "\r ©By"
+          sleep 0.5
+          #LWINGYI && YURI#53
+          # printf "\r ©By"
+          #sleep 0.5
+          #LWINGYI && YURI#54
+          printf "\r ©By L"
+          sleep 0.5
+          #LWINGYI && YURI#55
+          printf "\r ©By Lw"
+          sleep 0.5
+          #LWINGYI && YURI#56
+          printf "\r ©By Lwi"
+          sleep 0.5
+          #LWINGYI && YURI#57
+          printf "\r ©By Lwin"
+          sleep 0.5
+          #LWINGYI && YURI#58
+          printf "\r ©By LwinG"
+          sleep 0.5
+          #LWINGYI && YURI#59
+          printf "\r ©By LwinGy"
+          sleep 0.5
+          #LWINGYI && YURI#60
+          printf "\r ©By LwinGyi"
+          sleep 0.5
+          echo
+          #LWINGYI && YURI#61
+          printf "\r ©"
+          sleep 0.5
+          #LWINGYI && YURI#62
+          printf "\r ©By "
+          sleep 0.5
+          #LWINGYI && YURI#63
+          printf "\r ©By Y"
+          sleep 0.5
+          #LWINGYI && YURI#64
+          printf "\r ©By Yu"
+          sleep 0.5
+          #LWINGYI && YURI#65
+          printf "\r ©By YuR"
+          sleep 0.5
+          #LWINGYI && YURI#66
+          printf "\r ©By YuRi"
+          sleep 0.5
+          
+         echo
+
+echo -e "\e[92m╔════════════════════════════════════════════════════════════╗"
+echo -e "\e[92m║███╗░░░███╗░█████╗░███╗░░██╗░░░░░██╗░█████╗░██████╗░░█████╗░║"
+echo -e "\e[92m║████╗░████║██╔══██╗████╗░██║░░░░░██║██╔══██╗██╔══██╗██╔══██╗║"
+echo -e "\e[92m║██╔████╔██║███████║██╔██╗██║░░░░░██║███████║██████╔╝██║░░██║║"
+echo -e "\e[92m║██║╚██╔╝██║██╔══██║██║╚████║██╗░░██║██╔══██║██╔══██╗██║░░██║║"
+echo -e "\e[92m║██║░╚═╝░██║██║░░██║██║░╚███║╚█████╔╝██║░░██║██║░░██║╚█████╔╝║"
+echo -e "\e[92m║╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░║"
+echo -e "\e[92m╚══════════This Manjaro Linux ©By Lwin Gyi ♥️ YuRi ═══════════╝"
+
+sleep 6
+
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install wget -y 
+folder=manjaro-fs
+dlink="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Pacman/Manjaro"
+if [ -d "$folder" ]; then
+    first=1
+    echo "skipping downloading"
+fi
+tarball="manjaro.tar.xz"
+if [ "$first" != 1 ];then
+    wget --tries=20 https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Manjaro/manjaro.partaa -O manjaro.partaa
+    wget --tries=20 https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Manjaro/manjaro.partab -O manjaro.partab
+    wget --tries=20 https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Manjaro/manjaro.partac -O manjaro.partac
+    cat manjaro.parta* > manjaro.tar.xz
+    cur=`pwd`
+    mkdir -p "$folder"
+    cd "$folder"
+    echo "Decompressing Rootfs, please be patient."
+    proot --link2symlink tar -xf ${cur}/$tarball --exclude='dev'|| :
+    cd "$cur"
+fi
+mkdir -p manjaro-binds
+bin=start-manjaro.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A manjaro-binds)" ]; then
+    for f in manjaro-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b manjaro-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+#command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=en_US.UTF-8"
+command+=" LC_ALL=C"
+command+=" LANGUAGE=en_US"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
+echo "making $bin executable"
+chmod +x $bin
+echo "fixing permissions for manjaro"
+chmod 755 -R manjaro-fs
+
+#DE installation
+
+rm -rf $folder/root/.bash_profile
+wget $dlink/xfce4_de.sh -O $folder/root/xfce4_de.sh
+rm -rf $folder/etc/resolv.conf
+cat >$folder/etc/pacman.d/mirrorlist <<'EOL'
+##
+## Manjaro Linux repository mirrorlist
+## Generated on 02 May 2020 14:22
+##
+## Use pacman-mirrors to modify
+##
+## Location  : Germany
+## Time      : 99.99
+## Last Sync :
+Server = https://mirrors.dotsrc.org/manjaro-arm/stable/$repo/$arch
+EOL
+rm -rf $folder/etc/resolv.conf && echo "nameserver 1.1.1.1" > $folder/etc/resolv.conf
+wget --tries=20  $dlink/XFCE/vncserver-start -O $folder/usr/local/bin/vncserver-start 
+wget --tries=20 $dlink/XFCE/vncserver-stop -O $folder/usr/local/bin/vncserver-stop
+mkdir $folder/root/.vnc
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/Pacman/Manjaro/XFCE/xstartup -O $folder/root/.vnc/xstartup
+chmod +x $folder/usr/local/bin/vncserver-stop
+chmod +x $folder/usr/local/bin/vncserver-start
+echo " #!/bin/bash
+echo 'pacman-key --init && pacman-key --populate && pacman -Syu --noconfirm' > /usr/local/bin/fix-repo
+chmod +x /usr/local/bin/fix-repo
+fix-repo
+mkdir -p ~/.vnc
+clear
+if [ ! -f /root/xfce4_de.sh ]; then
+    wget --tries=20 $dlink/xfce4_de.sh -O /root/xfce4_de.sh
+    bash ~/xfce4_de.sh
+else
+    bash ~/xfce4_de.sh
+fi
+clear
+if [ ! -f /usr/bin/vncserver ]; then
+    pacman -S tigervnc --noconfirm > /dev/null
+fi
+clear
+rm -rf ~/.bash_profile" > $folder/root/.bash_profile 
+rm -rf manjaro.partaa manjaro.partab manjaro.partac manjaro.tar.xz manjaro.sh
+bash $bin
