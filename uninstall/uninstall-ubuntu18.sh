17f21b1f4cf37cdcb153b708127ab9c355c699de
lwingyiyuri <114618932+lwingyiyuri@users.noreply.github.com>
2023-01-26 21:49:56 +0630

    Create uninstall-ubuntu18.sh

---
+++ uninstall/uninstall-ubuntu18.sh
@@ -0,0 +1,178 @@
+sleep 5 &
+pid=$!
+
+          #LWINGYI && YURI#1
+          printf "\r S"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r St"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r Sta"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r Star"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r Start"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r Start U"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Un"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uni"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Unin"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Unins"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninst"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninsta"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstal"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall P"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Pl"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Ple"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Plea"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Pleas"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Please"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Please W"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Please Wa"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Please Wai"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Start Uninstall Please Wait"
+          sleep 0.5
+          
+          
+
+
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+
+chmod 777 -R ubuntu18-fs
+rm -rf ubuntu18-fs
+rm -rf ubuntu18-binds
+rm -rf ubuntu18.sh
+rm -rf start-ubuntu18.sh
+rm -rf ssh-apt.sh
+rm -rf de-apt.sh
+rm -rf de-apt-xfce4.sh
+rm -rf de-apt-mate.sh
+rm -rf de-apt-lxqt.sh
+rm -rf de-apt-lxde.sh
+rm -rf uninstall-ubuntu18.sh
+
+sleep 5 &
+pid=$!
+
+          #LWINGYI && YURI#1
+          printf "\r D"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r Do"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r Don"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r Done"
+          sleep 0.5
+          echo
+          sleep 5 &
+          pid=$!
+          #LWINGYI && YURI#5
+          printf "\r S"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r Sc"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Scr"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Scri"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Scrip"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script B"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©L"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©Lw"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©Lwi"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©Lwin"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinG"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGy"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi♥️"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi♥️Y"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi♥️Yu"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi♥️YuR"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Script By ©LwinGyi♥️YuRi"
+          sleep 0.5
+          echo
