08bf7ddc5229972d8472b11cc9a8e38d663cdf55
lwingyiyuri <114618932+lwingyiyuri@users.noreply.github.com>
2023-01-28 23:05:21 +0630

    Create ly-ssh-apt.sh

---
+++ desktop/ssh/ly-ssh-apt.sh
@@ -0,0 +1,133 @@
+sleep 5 &
+pid=$!
+          #LWINGYI && YURI#1
+          printf "\r W"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r We"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r Wel"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r Welc"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r Welco"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r Welcom"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome T"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To S"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SS"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH S"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH SE"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH SER"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH SERV"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH SERVE"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r Welcome  To SSH SERVER"
+          sleep 0.5
+          echo 
+          
+          
+          
+          
+sleep 5 &
+pid=$!
+          #LWINGYI && YURI#1
+          printf "\r 💖S"
+          sleep 0.5
+          #LWINGYI && YURI#2
+          printf "\r 💗SC"
+          sleep 0.5
+          #LWINGYI && YURI#3
+          printf "\r 💕SCR"
+          sleep 0.5
+          #LWINGYI && YURI#4
+          printf "\r 💓SCRI"
+          sleep 0.5
+          #LWINGYI && YURI#5
+          printf "\r 💗SCRIP"
+          sleep 0.5
+          #LWINGYI && YURI#6
+          printf "\r 💖SCRIPT"
+          sleep 0.5
+          #LWINGYI && YURI#7
+          printf "\r 💘SCRIPT©"
+          sleep 0.5
+          #LWINGYI && YURI#8
+          printf "\r 💗SCRIPT©B"
+          sleep 0.5
+          #LWINGYI && YURI#9
+          printf "\r 💝SCRIPT©BY"
+          sleep 0.5
+          #LWINGYI && YURI#10
+          printf "\r 💖SCRIPT©BY L"
+          sleep 0.5
+          #LWINGYI && YURI#11
+          printf "\r 💗SCRIPT©BY Lw"
+          sleep 0.5
+          #LWINGYI && YURI#12
+          printf "\r 💞SCRIPT©BY Lwi"
+          sleep 0.5
+          #LWINGYI && YURI#13
+          printf "\r 💗SCRIPT©BY Lwin"
+          sleep 0.5
+          #LWINGYI && YURI#14
+          printf "\r 💖SCRIPT©BY LwinG"
+          sleep 0.5
+          #LWINGYI && YURI#15
+          printf "\r 💗SCRIPT©BY LwinGy"
+          sleep 0.5
+          #LWINGYI && YURI#16
+          printf "\r 💖SCRIPT©BY LwinGyi"
+          sleep 0.5
+          #LWINGYI && YURI#17
+          printf "\r 💖SCRIPT©BY LwinGyi&"
+          sleep 0.5
+          #LWINGYI && YURI#18
+          printf "\r 💓SCRIPT©BY LwinGyi&&"
+          sleep 0.5
+          #LWINGYI && YURI#19
+          printf "\r 💕SCRIPT©BY LwinGyi&&Y"
+          sleep 0.5
+          #LWINGYI && YURI#20
+          printf "\r 💓SCRIPT©BY LwinGyi&&Yu"
+          sleep 0.5
+          #LWINGYI && YURI#21
+          printf "\r 💖SCRIPT©BY LwinGyi&&YuR"
+          sleep 0.5
+          #LWINGYI && YURI#22
+          printf "\r 💗SCRIPT©BY LwinGyi&&YuRi"
+          sleep 0.5
+          #LWINGYI && YURI#23
+          printf "\r 💖SCRIPT©BY LwinGyi&&YuRi "
+          sleep 0.5
+          echo
